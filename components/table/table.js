123952cca80f75bf3088be58b346ab0701b29c46
wanghaoxin <a706519033@qq.com>
2019-08-14 11:46:26 +0800

    table.js

--- components/table/table.js
+++ components/table/table.js
@@ -1,26 +1,26 @@
 Component({
   properties: {
-    /**
-     * 表格数据，需要与titles的key相对应
+    /** 
+     * 表格数据，需要与titles的key相对应 
      */
-    data: { 
-      type:Array
+    data: {
+      type: Array
     },
-    /**
-     * 表格头
-     * {date:'日期', name:'名字',.......}
+    /** 
+     * 表格头 
+     * {date:'日期', name:'名字',.......} 
      * */
     titles: {
       type: Object
     },
-    /**
-     * 表格标题
+    /** 
+     * 表格标题 
      */
     caption: {
       type: String
     },
-    /**
-     * 单元格宽度 px
+    /** 
+     * 单元格宽度 px 
     */
     colWidth: {
       type: Number,
@@ -30,7 +30,7 @@ Component({
 
   lifetimes: {
     attached: function () {
-      //根据titles 动态设置表格的宽度
+      //根据titles 动态设置表格的宽度 
       let keys = Object.keys(this.data.titles);
       this.setData({
         tableWidth: keys.length * this.properties.colWidth
@@ -38,9 +38,9 @@ Component({
     },
   },
   data: {
-    
+
   },
   methods: {
-   
+
   }
-})
+}) 
